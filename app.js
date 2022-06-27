15ceb738f4b062d9cd616b1e2a124822f10a99e4
Sadet Turhan <msadetturhan1@gmail.com>
2022-06-27 15:23:22 +0300

    final

--- app.js
+++ app.js
@@ -1,67 +1,20 @@
-const numarea = document.querySelector(".numberarea");
-const numbtn1 = document.querySelector(".btn1");
-const numbtn2 = document.querySelector(".btn2");
-const numbtn3 = document.querySelector(".btn3");
-const numbtn4 = document.querySelector(".btn4");
-const numbtn5 = document.querySelector(".btn5");
-const numbtn6 = document.querySelector(".btn6");
-const numbtn7 = document.querySelector(".btn7");
-const numbtn8 = document.querySelector(".btn8");
-const numbtn9 = document.querySelector(".btn9");
-const numbtn0 = document.querySelector(".btn0");
+let display = document.getElementById('display');
+let buttons = Array.from(document.getElementsByClassName('buttons'));
 
-
-numbtn1.addEventListener('click', (event) => {
-    const numbtn1 = document.createTextNode('1');
-    numarea.appendChild(numbtn1);
+buttons.map( button => {
+    button.addEventListener('click', (e) =>{
+        switch(e.target.innerText){
+            case 'C':
+                display.innerText = '';
+                break;
+            case 'del':
+                display.innerText = display.innerText.slice(0, -1);
+                break;
+            case '=':
+                display.innerText = eval(display.innerText);
+                break;
+            default:
+                display.innerText += e.target.innerText;
+        }
+    });
 });
-
-numbtn2.addEventListener('click', (event) => {
-    const numbtn2 = document.createTextNode('2');
-    numarea.appendChild(numbtn2);
-});
-
-numbtn3.addEventListener('click', (event) => {
-    const numbtn3 = document.createTextNode('3');
-    numarea.appendChild(numbtn3);
-});
-
-numbtn4.addEventListener('click', (event) => {
-    const numbtn4 = document.createTextNode('4');
-    numarea.appendChild(numbtn4);
-});
-
-numbtn5.addEventListener('click', (event) => {
-    const numbtn5 = document.createTextNode('5');
-    numarea.appendChild(numbtn5);
-});
-
-numbtn6.addEventListener('click', (event) => {
-    const numbtn6 = document.createTextNode('6');
-    numarea.appendChild(numbtn6);
-});
-
-numbtn7.addEventListener('click', (event) => {
-    const numbtn7 = document.createTextNode('7');
-    numarea.appendChild(numbtn7);
-});
-
-numbtn8.addEventListener('click', (event) => {
-    const numbtn8 = document.createTextNode('8');
-    numarea.appendChild(numbtn8);
-});
-
-numbtn9.addEventListener('click', (event) => {
-    const numbtn9 = document.createTextNode('9');
-    numarea.appendChild(numbtn9);
-});
-
-numbtn0.addEventListener('click', (event) => {
-    const numbtn0 = document.createTextNode('0');
-    numarea.appendChild(numbtn0);
-});
-
-const clear = document.querySelector(".btn-clear");
-
-
-
