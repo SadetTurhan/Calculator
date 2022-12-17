dcd7546ddb1834d089abef3b5d9bd97586a97a8c
Sadet Turhan <msadetturhan1@gmail.com>
2022-12-17 12:31:15 +0300

    main functions added

--- app.js
+++ app.js
@@ -1,163 +1,132 @@
-let result = [];
+//operators//
 let operator = [];
-let firstNumber = [];
-let secondNumber = [];
 
-function operatingNumbers(){
-if(operator == "add"){
-    let z = firstNumber.join("") + secondNumber.join();
-    result.push(z)
-    let y = result[0] + result[1]
-    result.push(y)
-    
-}else if(operator == "substract"){
-    let z = x - y;
-    result.push(z);
-}else if(operator == "multiply"){
-    let z = x * y;
-    result.push(z);
-}else if(operator == "divide"){
-    let z = x / y;
-    result.push(z);
-}
-};
-function chooseAdd(){
-    let pushAdd = "add";
-    operator.pop();
-    operator.push(pushAdd);
-}
-function chooseSubstract(){
-    let pushSubstract = "substract";
-    operator.pop();
-    operator.push(pushSubstract);
-}
-function chooseMultiply(){
-    let pushMultiply = "multiply";
-    operator.pop();
-    operator.push(pushMultiply);
-}
-function chooseDivide(){
-    let pushDivide = "divide";
-    operator.pop();
-    operator.push(pushDivide);
-}
+const addButton = document.getElementById("add");
+const substractButton = document.getElementById("substract");
+const multiplyButton = document.getElementById("multiply");
+const divideButton = document.getElementById("divide");
 
-let addButton = document.querySelector(".add");
-let substractButton = document.querySelector(".substract");
-let multiplyButton = document.querySelector(".multiply");
-let divideButton = document.querySelector(".divide");
-addButton.addEventListener("click",chooseAdd);
-substractButton.addEventListener("click",chooseSubstract);
-multiplyButton.addEventListener("click",chooseMultiply);
-divideButton.addEventListener("click",chooseDivide);
+//choosing operator//
+addButton.addEventListener("click",function(){
+    operator.pop();
+    operator.push("add");
+})
+substractButton.addEventListener("click",function(){
+    operator.pop();
+    operator.push("substract");
+})
+multiplyButton.addEventListener("click",function(){
+    operator.pop();
+    operator.push("multiply");
+})
+divideButton.addEventListener("click",function(){
+    operator.pop();
+    operator.push("divide");
+})
 
-
-let numberDisplay = document.querySelector(".displayArea");
-let numberDisplayValue = document.querySelector(".resultText").nodeValue;
-let number1 = document.querySelector(".numberButton1");
-number1.addEventListener("click",function(){
+//Buttons part//
+const num1 = document.getElementById("numberButton1");
+const num2 = document.getElementById("numberButton2");
+const num3 = document.getElementById("numberButton3");
+const num4 = document.getElementById("numberButton4");
+const num5 = document.getElementById("numberButton5");
+const num6 = document.getElementById("numberButton6");
+const num7 = document.getElementById("numberButton7");
+const num8 = document.getElementById("numberButton8");
+const num9 = document.getElementById("numberButton9");
+const num0 = document.getElementById("numberButton0");
+let displayArea = document.getElementById("secondNumber");
+let upperArea = document.getElementById("firstNumber");
+num1.addEventListener("click",function(){
     if(operator == "add" || operator == "substract" || operator == "multiply" || operator == "divide"){
-    secondNumber.push(1);
-    numberDisplay.textContent = secondNumber.join("");}
+    upperArea.textContent += 1;}
     else{
-    firstNumber.push(1);
-    numberDisplay.textContent = firstNumber.join("");
+    displayArea.textContent +=1
     };
-})
-let number2 = document.querySelector(".numberButton2");
-number2.addEventListener("click",function(){
-        if(operator == "add" || operator == "substract" || operator == "multiply" || operator == "divide"){
-        secondNumber.push(2);
-        numberDisplay.textContent = secondNumber.join("");}
+});
+num2.addEventListener("click",function(){
+    if(operator == "add" || operator == "substract" || operator == "multiply" || operator == "divide"){
+        upperArea.textContent += 2;}
         else{
-        firstNumber.push(2);
-        numberDisplay.textContent = firstNumber.join("");
+        displayArea.textContent +=2
         };
-});
-let number3 = document.querySelector(".numberButton3");
-number3.addEventListener("click",function(){
-    if(operator == "add" || operator == "substract" || operator == "multiply" || operator == "divide"){
-    secondNumber.push(3);
-    numberDisplay.textContent = secondNumber.join("");}
-    else{
-    firstNumber.push(3);
-    numberDisplay.textContent = firstNumber.join("");
-    }});
-let number4 = document.querySelector(".numberButton4");
-number4.addEventListener("click",function(){
-    if(operator == "add" || operator == "substract" || operator == "multiply" || operator == "divide"){
-        secondNumber.push(4);
-        numberDisplay.textContent = secondNumber.join("");}
-        else{
-        firstNumber.push(4);
-        numberDisplay.textContent = firstNumber.join("");
-        }
 })
-let number5 = document.querySelector(".numberButton5");
-number5.addEventListener("click",function(){
+num3.addEventListener("click",function(){
     if(operator == "add" || operator == "substract" || operator == "multiply" || operator == "divide"){
-        secondNumber.push(5);
-        numberDisplay.textContent = secondNumber.join("");}
+        upperArea.textContent += 3;}
         else{
-        firstNumber.push(5);
-        numberDisplay.textContent = firstNumber.join("");
-        }
+        displayArea.textContent +=3
+        };
 })
-let number6 = document.querySelector(".numberButton6");
-number6.addEventListener("click",function(){
+num4.addEventListener("click",function(){
     if(operator == "add" || operator == "substract" || operator == "multiply" || operator == "divide"){
-        secondNumber.push(6);
-        numberDisplay.textContent = secondNumber.join("");}
+        upperArea.textContent += 4;}
         else{
-        firstNumber.push(6);
-        numberDisplay.textContent = firstNumber.join("");
-        }
+        displayArea.textContent +=4
+        };
 })
-let number7 = document.querySelector(".numberButton7");
-number7.addEventListener("click",function(){
+num5.addEventListener("click",function(){
     if(operator == "add" || operator == "substract" || operator == "multiply" || operator == "divide"){
-        secondNumber.push(7);
-        numberDisplay.textContent = secondNumber.join("");}
+        upperArea.textContent += 5;}
         else{
-        firstNumber.push(7);
-        numberDisplay.textContent = firstNumber.join("");
-        }
+        displayArea.textContent +=5
+        };
 })
-let number8 = document.querySelector(".numberButton8");
-number8.addEventListener("click",function(){
+num6.addEventListener("click",function(){
     if(operator == "add" || operator == "substract" || operator == "multiply" || operator == "divide"){
-        secondNumber.push(8);
-        numberDisplay.textContent = secondNumber.join("");}
+        upperArea.textContent += 6;}
         else{
-        firstNumber.push(8);
-        numberDisplay.textContent = firstNumber.join("");
-        }
+        displayArea.textContent +=6
+        };
 })
-let number9 = document.querySelector(".numberButton9");
-number9.addEventListener("click",function(){
+num7.addEventListener("click",function(){
     if(operator == "add" || operator == "substract" || operator == "multiply" || operator == "divide"){
-        secondNumber.push(9);
-        numberDisplay.textContent = secondNumber.join("");}
+        upperArea.textContent += 7;}
         else{
-        firstNumber.push(9);
-        numberDisplay.textContent = firstNumber.join("");
-        }
-});
-let number0 = document.querySelector(".numberButton0");
-number0.addEventListener("click",function(){
+        displayArea.textContent +=7
+        };
+})
+num8.addEventListener("click",function(){
     if(operator == "add" || operator == "substract" || operator == "multiply" || operator == "divide"){
-        secondNumber.push(0);
-        numberDisplay.textContent = secondNumber.join("");}
+        upperArea.textContent += 8;}
         else{
-        firstNumber.push(0);
-        numberDisplay.textContent = firstNumber.join("");
-        }
-});
+        displayArea.textContent +=8
+        };
+})
+num9.addEventListener("click",function(){
+    if(operator == "add" || operator == "substract" || operator == "multiply" || operator == "divide"){
+        upperArea.textContent += 9;}
+        else{
+        displayArea.textContent +=9
+        };
+})
+num0.addEventListener("click",function(){
+    if(operator == "add" || operator == "substract" || operator == "multiply" || operator == "divide"){
+        upperArea.textContent += 0;}
+        else{
+        displayArea.textContent +=0
+        };
+})
 
 
-let equalsButton = document.querySelector(".equals");
+//getting values//
 
+var getDisplayArea = 0;
+var getUpperArea = 0;
+var result = 0;
+const equalsButton = document.getElementById("equals");
 equalsButton.addEventListener("click",function(){
-    operatingNumbers(firstNumber,secondNumber);
-    numberDisplay.textContent = result;
-})
+    getDisplayArea = parseInt(displayArea.textContent);
+    getUpperArea = parseInt(upperArea.textContent);
+    if(operator == "add"){
+    result = getDisplayArea + getUpperArea;
+    }else if(operator == "substract"){
+    result = getDisplayArea - getUpperArea;
+    }else if(operator == "multiply"){
+    result = getDisplayArea * getUpperArea;
+    }else if(operator == "divide"){
+    result = getDisplayArea / getUpperArea;
+    };
+    upperArea.textContent = "";
+    displayArea.textContent = result;
+});
